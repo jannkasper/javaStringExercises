5f08c83ac7dcb10fc4d6ad57fb34f4862aa05801
Jan K <jann.kasper@gmail.com>
2019-05-14 05:53:41 +0200

    4. Anagram Algorithm

--- src/stringExercises/Anagram.java
+++ src/stringExercises/Anagram.java
@@ -1,8 +1,20 @@
 package stringExercises;
 
-public class Anagram {
-    public boolean checkAnagram (String word1, String word2) {
+import java.util.Arrays;
 
-        return  true;
+public class Anagram {
+    public boolean checkAnagram (String text1, String text2) {
+
+        if (text1 == null || text1.isEmpty() || text2 == null || text2.isEmpty())
+            return  false;
+
+        char[] arr1 = text1.toLowerCase().replaceAll("[^a-zA-Z0-9]","").toCharArray();
+        char[] arr2 = text2.toLowerCase().replaceAll("[^a-zA-Z0-9]", "").toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return (String.copyValueOf(arr1).equals(String.copyValueOf(arr2))) ? true : false;
+
     }
 }
--- src/testing/AnagramTest.java
+++ src/testing/AnagramTest.java
@@ -25,19 +25,19 @@ public class AnagramTest {
 
     @Test
     public void test3() {
-        assertEquals(true, anagram.checkAnagram("The Morse Code", "Here comes dots"));
+        assertEquals(true, anagram.checkAnagram("Fourth of July", "Joyful Fourth"));
     }
     @Test
     public void test4() {
-        assertEquals(true, anagram.checkAnagram("The Morse Codee", "Here comes dots"));
+        assertEquals(false, anagram.checkAnagram("The Morse Code", "Here comes dots"));
     }
     @Test
     public void test5() {
-        assertEquals(true, anagram.checkAnagram("", "Here comes dots"));
+        assertEquals(false, anagram.checkAnagram("", "Here comes dots"));
     }
     @Test
     public void test6() {
-        assertEquals(true, anagram.checkAnagram("The Morse Code", null));
+        assertEquals(false, anagram.checkAnagram("The Morse Code", null));
     }
 
 
